6ae83b27bef6b81fcaa9a08d8bb7ac57218d014f
hosseinirtr <h.hosseini.me@gmail.com>
2025-05-24 00:45:27 +0330

    refactor: replace div with fragment and add ids to sections
    Improve semantic structure by replacing the wrapping div with a React fragment and adding ids to key sections for better navigation and accessibility.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,6 @@
 import { useTranslations } from "next-intl";
 import AboutMe from "./about_me/page";
 import Contact from "./contact/page";
-// import Work from "./project/page";
 import Projects from "./project/page";
 import Resume from "./resume/page";
 import Services from "./services/page";
@@ -15,9 +14,9 @@ export default function RootPage({
   const t = useTranslations("Home");
 
   return (
-    <div>
+    <>
       <main className="flex flex-col">
-        <div className="min-h-screen snap-center">
+        <div className="min-h-screen snap-center" id="#about-me">
           <AboutMe />
         </div>
 
@@ -30,16 +29,16 @@ export default function RootPage({
           <h2 className="text-4xl text-center">{t("Services")}</h2>
           <Services />
         </div>
-        <div className="min-h-screen snap-center">
+        <div className="min-h-screen snap-center" id="#projects">
           <h2 className="text-4xl text-center my-8">{t("Projects")}</h2>
           <Projects />
         </div>
 
-        <div className="min-h-screen snap-center">
+        <div className="min-h-screen snap-center" id="#contact">
           <h2 className="text-4xl text-center my-8">{t("Contact")}</h2>
           <Contact />
         </div>
       </main>
-    </div>
+    </>
   );
 }
