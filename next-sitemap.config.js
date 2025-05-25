8d39d5dd35fd998f659cda1dc4218c4ef88908f4
hosseinirtr <h.hosseini.me@gmail.com>
2025-05-25 23:02:25 +0330

    refactor: 📈 Change content for improve seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,34 @@
 module.exports = {
-  siteUrl: "https://hosseinhosseini.net",
+  siteUrl: "https://hosseini-rtr.ir",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/404"],
+  alternateRefs: [
+    {
+      href: 'https://hosseini-rtr.ir/en',
+      hreflang: 'en'
+    },
+    {
+      href: 'https://hosseini-rtr.ir/fa',
+      hreflang: 'fa'
+    },
+    {
+      href: 'https://hosseini-rtr.ir/it',
+      hreflang: 'it'
+    }
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/404']
+      }
+    ],
+    additionalSitemaps: [
+      'https://hosseini-rtr.ir/sitemap.xml'
+    ]
+  }
 };
